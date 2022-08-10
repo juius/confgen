00cc990fb6a38e874c069da567494c4ba0e807c7
juius <hwg245@alumni.ku.dk>
2022-08-10 10:16:54 +0200

    fix import

--- confgen/__init__.py
+++ confgen/__init__.py
@@ -1,5 +1,6 @@
 import confgen
 from confgen.crest import CREST
 from confgen.etkdg import ETKDG
+from confgen.generator import MixedGenerator
 from confgen.obabel import Confab, Gen3d, WeightedRotor
 from confgen.tools import Cluster, Filter, GeomOptimizer, SinglePoint
